6563c7f5fc2bc9712310c41a5a5fd278ab6560d1
Rohan <rohan26gupta2005@gmail.com>
2026-05-06 17:26:32 +0530

    added web ui

--- web_api/server.py
+++ web_api/server.py
@@ -36,6 +36,7 @@ app = FastAPI(title="Mini-Dropbox API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
